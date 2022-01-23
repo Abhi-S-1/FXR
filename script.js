71679b9b33a58aca1569daa23683c04261408747
Alex <alextheprogrammer21@gmail.com>
2022-01-23 08:40:38 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  $("#modal-trigger").click(function (e) {
+    e.preventDefault();
+    dataModal = $(this).attr("data-modal");
+    $("#" + dataModal).css({ display: "block" });
+  });
+
+  $(".close-modal, .modal-sandbox").click(function () {
+    $(".modal").css({ display: "none" });
+  });
+});
